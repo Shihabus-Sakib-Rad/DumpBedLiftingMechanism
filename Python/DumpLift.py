ca7e559eee0db44779822b106d98449a7f6c790b
Shihabus-Sakib-Rad <2k4ieujtq@relay.firefox.com>
2021-08-26 05:04:15 +0600

    Major BUG fixed.
    Previously, all the link lengths were given but was never verified if the links were physically possible to arrange. Now, l3 and l5 are given, l4 is automatically calculated.

--- Python/DumpLift.py
+++ Python/DumpLift.py
@@ -1,15 +1,22 @@
 from math import acos, cos, sqrt, pi
 
 
-def x(a, b, c):
-    return (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
+def get_l2r_l4(l3, l5, x51, f):
+    l4 = trig_arm(l5-l3, l5, x51)
+    x41cw = trig_angle(l5-l3, l4, l5)
+    x41ccw = 180 - x41cw
+    l4b = f*l4
+    l4a = l4 - l4b
+    l2r = trig_arm(l3, l4b, x41ccw)
+    return l2r, l4, l4a, l4b
 
 
 def trig_angle(a, b, c):
-    return acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
+    return rad_to_deg(acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b)))
 
 
 def trig_arm(a, b, C):
+    C = deg_to_rad(C)
     return sqrt(a ** 2 + b ** 2 - 2 * a * b * cos(C))
 
 
@@ -17,67 +24,45 @@ def rad_to_deg(x):
     return (180 * x) / pi
 
 
+def deg_to_rad(x):
+    return (pi * x) / 180
+
+
 class DumpLift:
-    def __init__(self, l1, l2, l3, l4, l5, f):
+    def __init__(self, l1, l2, l3, l4a, l4b, l5):
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
-        self.l4 = l4
+        self.l4a = l4a
+        self.l4b = l4b
         self.l5 = l5
-        self.f = f
+        self.l4 = l4a + l4b
 
         # intermediate vars
-        self._a43 = trig_angle(self.l3, (self.f * self.l4), self.l2)
+        self._a43 = trig_angle(self.l3, self.l4b, self.l2)
         self._l7 = trig_arm(self.l3, self.l4, self._a43)
         self._a51 = trig_angle(self.l1, self.l5, self._l7)
         self._a57 = trig_angle(self.l5, self._l7, self.l1)
         self._a47 = trig_angle(self.l4, self._l7, self.l3)
         self._a54 = self._a57 - self._a47
         self._l8 = trig_arm(self.l5, self.l4, self._a54)
-        self._a23 = trig_angle(self.l2, self.l3, (self.f * self.l4))
+        self._a23 = trig_angle(self.l2, self.l3, self.l4b)
 
         # calculate angles in radians
         self.a3 = trig_angle(self.l1, self.l3, self._l8)
         self.a2 = self._a23 + self.a3
-        self.a4 = (pi - self._a43) + self.a3
-        self.a5 = pi - self._a51
-
-    def a2_rad(self):
-        return self.a2
-
-    def a3_rad(self):
-        return self.a3
-
-    def a4_rad(self):
-        return self.a4
-
-    def a5_rad(self):
-        return self.a5
-
-    def a2_deg(self):
-        return rad_to_deg(self.a2)
-
-    def a3_deg(self):
-        return rad_to_deg(self.a3)
-
-    def a4_deg(self):
-        return rad_to_deg(self.a4)
-
-    def a5_deg(self):
-        return rad_to_deg(self.a5)
-
-    def angles_rad(self):
-        return [self.a2, self.a3, self.a4, self.a5]
+        self.a4 = (180 - self._a43) + self.a3
+        self.a5 = 180 - self._a51
 
     def angles_deg(self):
-        result = []
-        rad_list = self.angles_rad()
-        for ran_ang in rad_list:
-            result.append(rad_to_deg(ran_ang))
+        return [self.a2, self.a3, self.a4, self.a5]
 
-        return result
-
-
-if __name__ == '__main__':
-    print(x(0.7, (0.3 * 0.5), 0.6))
-    print(trig_angle(0.7, (0.3 * 0.5), 0.6))
+    def print_all(self):
+        r = [self._l7, self._l8]
+        r2 = [self._a23, self._a43, self._a51, self._a54, self._a57, self._a47]
+        for i in r:
+            print(f"{i:.2f}")
+        for i in r2:
+            print(f"{i:.2f}")
+        for i in self.angles_deg():
+            print(f"{i:.2f}")
--- Python/main.py
+++ Python/main.py
@@ -1,21 +1,24 @@
-from DumpLift import DumpLift
+from DumpLift import DumpLift, get_l2r_l4
 
 
 def print_angles(l2, angs: list[float]):
-    links = ["l2", "l3", "l4", "l5"]
     ang_labels = ["θ2", "θ3", "θ4", "θ5"]
     print(f"l2:{l2:6.2f} cm\tAngles (deg):", end="\t")
     for i, ang in enumerate(angs):
         print(f"{ang_labels[i]}: {ang:.2f}", end="\t")
-    # print(f"Output Angle: {(180 - angs[3]):.2f} deg")
+    print(f"Output Angle: {(180 - angs[3]):.2f} deg")
     print()
 
 
 if __name__ == '__main__':
-    l1, l2, l3, l4, l5, f = 200, 120, 104, 120, 200, 0.2  # cm
+    tilt_angle = 5
+    l3, l5, f = 100, 200, 0.2  # cm
+    l1 = l5
+    l2r, l4, l4a, l4b = get_l2r_l4(l3, l5, tilt_angle, f)
+    print(l2r, l4, l4a, l4b)
+    l2_min = round(l2r) + 1
+    l2_max = round(l3+l4b) - 1
 
-    l2_min: float = l3 - f * l4
-    l2_max: float = l3 + f * l4
-    for l2 in range(round(l2_min + 1), round(l2_max - 1)):
-        position = DumpLift(l1, l2, l3, l4, l5, f)
+    for l2 in range(l2_min, l2_max):
+        position = DumpLift(l1, l2, l3, l4a, l4b, l5)
         print_angles(l2, position.angles_deg())
